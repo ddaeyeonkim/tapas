4c0d22a609c7f15eafaf5130b8ee1fe5a78371b1
improve777 <improve777@gmail.com>
2021-07-21 01:25:22 +0900

    BrowseActivity 헤더영역에 모션 레이아웃 적용

--- app/src/main/java/com/improve777/tapas/BrowseActivity.kt
+++ app/src/main/java/com/improve777/tapas/BrowseActivity.kt
@@ -1,6 +1,11 @@
 package com.improve777.tapas
 
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import com.improve777.tapas.base.BaseActivity
 import com.improve777.tapas.databinding.ActivityBrowseBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -10,5 +15,23 @@ class BrowseActivity : BaseActivity<ActivityBrowseBinding>(ActivityBrowseBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        clearStatusBarColor()
+        fetchHeaderWindowInsets()
+    }
+
+    private fun clearStatusBarColor() {
+        window.apply {
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
+    }
+
+    private fun fetchHeaderWindowInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.appbarLayout) { _, insets ->
+            val statusBarHeight = insets.getInsets(WindowInsetsCompat.Type.statusBars()).top
+            binding.appbarLayout.updateLayoutParams { height += statusBarHeight }
+            binding.toolbar.ctBrowse.minimumHeight += statusBarHeight
+            insets
+        }
     }
 }
